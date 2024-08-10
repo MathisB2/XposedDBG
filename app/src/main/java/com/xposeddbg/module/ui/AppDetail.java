7d2b7b844d0a4f86e68a47ebf99ac28d770a1eed
MathisB2 <mathis.bodin2@gmail.com>
2024-08-10 17:58:41 +0200

    refactor

--- app/src/main/java/com/xposeddbg/module/ui/AppDetail.java
+++ app/src/main/java/com/xposeddbg/module/ui/AppDetail.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,8 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.LongDef;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,14 +19,14 @@ import androidx.core.view.WindowInsetsCompat;
 import com.xposeddbg.module.DexClassExplorer;
 import com.xposeddbg.module.R;
 import com.xposeddbg.module.model.AppInfo;
+import com.xposeddbg.module.model.hooks.HookInfo;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AppDetail extends AppCompatActivity {
-    private AppInfo app;
+    private AppInfo currentApp;
     private ImageView appIcon;
     private TextView appTitle;
 
@@ -54,9 +51,9 @@ public class AppDetail extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        this.app = getIntent().getSerializableExtra("appInfo", AppInfo.class);
+        this.currentApp = getIntent().getSerializableExtra("appInfo", AppInfo.class);
         this.appTitle = findViewById(R.id.appDetail_title);
-        this.appTitle.setText(this.app.getAppName());
+        this.appTitle.setText(this.currentApp.getAppName());
 
         this.appIcon = findViewById(R.id.appDetail_icon);
 
@@ -64,17 +61,17 @@ public class AppDetail extends AppCompatActivity {
 
         PackageManager pm = getPackageManager();
         try {
-            Drawable icon = pm.getApplicationIcon(this.app.getPackageName());
-            this.app.setIcon(icon);
+            Drawable icon = pm.getApplicationIcon(this.currentApp.getPackageName());
+            this.currentApp.setIcon(icon);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        this.appIcon.setImageDrawable(this.app.getIcon());
+        this.appIcon.setImageDrawable(this.currentApp.getIcon());
 
 
 
-        Intent launchIntent = pm.getLaunchIntentForPackage(this.app.getPackageName());
-        this.app.setLaunchIntent(launchIntent);
+        Intent launchIntent = pm.getLaunchIntentForPackage(this.currentApp.getPackageName());
+        this.currentApp.setLaunchIntent(launchIntent);
 
         this.launchButton = findViewById(R.id.appDetail_startButton);
         this.launchButton.setOnClickListener(new View.OnClickListener() {
@@ -83,7 +80,7 @@ public class AppDetail extends AppCompatActivity {
                 launchApp();
             }
         });
-        this.launchButton.setEnabled(this.app.isLaunchable());
+        this.launchButton.setEnabled(this.currentApp.isLaunchable());
         this.launchButton.setAllowClickWhenDisabled(true);
 
         this.stopButton = findViewById(R.id.appDetail_stopButton);
@@ -96,24 +93,24 @@ public class AppDetail extends AppCompatActivity {
 
 
         this.packageName = findViewById(R.id.appDetail_packageName);
-        this.packageName.setText(this.app.getPackageName());
+        this.packageName.setText(this.currentApp.getPackageName());
 
         this.versionName = findViewById(R.id.appDetail_versionName);
-        this.versionName.setText(this.app.getVersion());
+        this.versionName.setText(this.currentApp.getVersion());
 
         this.apkPath = findViewById(R.id.appDetail_apkDirectory);
-        this.apkPath.setText(this.app.getApkDirectory());
+        this.apkPath.setText(this.currentApp.getApkDirectory());
 
 
-        List<String> methodNames = DexClassExplorer.getMethods(this.app);
+        List<HookInfo> hookableMethods = DexClassExplorer.getMethods(this.currentApp);
     }
 
 
 
 
     private void launchApp() {
-        if (this.app.isLaunchable()) {
-            startActivity(this.app.getLaunchIntent());
+        if (this.currentApp.isLaunchable()) {
+            startActivity(this.currentApp.getLaunchIntent());
         } else {
             Toast.makeText(this, "Unable to launch this app", Toast.LENGTH_SHORT).show();
         }
@@ -124,7 +121,7 @@ public class AppDetail extends AppCompatActivity {
         try {
             Process su = Runtime.getRuntime().exec("su");
             DataOutputStream os = new DataOutputStream(su.getOutputStream());
-            os.writeBytes("am force-stop " + this.app.getPackageName() + "\n");
+            os.writeBytes("am force-stop " + this.currentApp.getPackageName() + "\n");
             os.flush();
             os.writeBytes("exit\n");
             os.flush();
